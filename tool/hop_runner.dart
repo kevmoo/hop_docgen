3b075cfbd9a162bd9e5429c11d2f1919757a57d1
Kevin Moore <github@j832.com>
2014-03-16 11:30:39 -0700

    updated unittest timeout to 2 minutes

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -7,8 +7,8 @@ import 'package:hop_docgen/hop_docgen.dart';
 import '../test/harness_console.dart' as test;
 
 void main(List<String> args) {
-
-  addTask('test', createUnitTestTask(test.main));
+  addTask('test', createUnitTestTask(test.main,
+      timeout: const Duration(minutes: 2)));
 
   addTask('docs',  createDocGenTask('../kev_dartdoc_viewer'));
 
